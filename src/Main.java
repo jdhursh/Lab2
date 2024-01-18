35c5ebb6ad5e4d3ce7d8f4a3a226d9a4592458ee
Jesse <jdhursh@eiu.edu>
2024-01-18 11:16:47 -0600

    Learning how to use delimiters in the Scanner class

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int birthday[] = userInput();
+        System.out.println("Month: " + birthday[0]);
+        System.out.println("Day: " + birthday[1]);
+        System.out.println("Year: " + birthday[2]);
     }
 
-    public static int userInput() {
-        Scanner console = new Scanner(System.in);
+    public static int[] userInput() {
+        Scanner console = new Scanner(System.in).useDelimiter("\\s*/\\s*");
 
         System.out.println("Please enter your date of birth: ");
+        int month = console.nextInt();
+        int day = console.nextInt();
+        int year = console.nextInt();
 
-        return console.nextInt();
+        return new int[]{month, day, year};
     }
 }

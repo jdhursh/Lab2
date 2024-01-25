c425df0dfa01b70ccf16f472ce4bcd23a46df3d2
Jesse <jdhursh@eiu.edu>
2024-01-24 23:28:11 -0600

    name and length variables

---
+++ src/Name.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+
+public class Name {
+    public static void main(String[] args){
+        String name = "Jesse".toLowerCase();
+        int length = name.length();
+
+        System.out.println(name);
+    }
+}
